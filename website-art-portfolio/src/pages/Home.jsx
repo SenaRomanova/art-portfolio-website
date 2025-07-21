6fc0e09c289c8aae319d2a7d6f654a7d1fa136e2
SenaRomanova <gxkseniaromanova@gmail.com>
2025-07-21 15:56:07 -0400

    some experimental changes to the home page

--- website-art-portfolio/src/pages/Home.jsx
+++ website-art-portfolio/src/pages/Home.jsx
@@ -1,10 +1,23 @@
-import Box
- from "@mui/material/Box";
+import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
+
 export default function Home() {
-    return(
-        <Box sx={{bgcolor: '#66666686', display: 'flex', height: '100vh', justifyContent: "center"}}>
-            <Typography variant="h2"> Home </Typography>
-        </Box>
+    return (
+        <>
+        <Typography variant="h2" color="grey"> Home </Typography>
+
+        <Container sx={{ margin: '16px', bgcolor: '#ffa0ff', display: 'flex', height: '50vh', justifyContent: "center",  alignItems: "center" }}>
+                    
+            <Container sx={{ margin: '16px', width: 'inherit', height: 'inherit', bgcolor: '#000000' }}></Container>
+            <Container sx={{ margin: '16px', width: 'inherit', height: 'inherit', bgcolor: '#000000' }}></Container>
+        </Container>
+
+        <Container sx={{ margin: '16px', bgcolor: '#ffa0ff', display: 'flex', height: '50vh', justifyContent: "center",  alignItems: "center" }}>
+                    
+            <Container sx={{ margin: '16px', width: 'inherit', height: 'inherit', bgcolor: '#000000' }}></Container>
+            <Container sx={{ margin: '16px', width: 'inherit', height: 'inherit', bgcolor: '#000000' }}></Container>
+        </Container>
+        </>
+        
     );
 }
